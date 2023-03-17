a975938bd1d81eef48b5b81daf535944532adb3a
jimzer <jimi.vaubien@protonmail.com>
2023-03-18 00:01:04 +0100

    Handle single user mode with the Credentials provider

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -4,7 +4,88 @@ import { getServerSession } from "next-auth";
 import { prisma } from "@/server/db";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { type AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import EmailProvider from "next-auth/providers/email";
+
+const emailProvider = EmailProvider({
+  server: {
+    host: process.env.EMAIL_SERVER_HOST,
+    port: process.env.EMAIL_SERVER_PORT
+      ? parseInt(process.env.EMAIL_SERVER_PORT)
+      : undefined,
+    auth: {
+      user: process.env.EMAIL_SERVER_USER,
+      pass: process.env.EMAIL_SERVER_PASSWORD,
+    },
+    secure: true,
+  },
+  from: process.env.EMAIL_FROM,
+
+  normalizeIdentifier(identifier: string): string {
+    // Get the first two elements only,
+    // separated by `@` from user input.
+    // eslint-disable-next-line prefer-const
+    let [local, domain] = identifier.toLowerCase().trim().split("@");
+    // The part before "@" can contain a ","
+    // but we remove it on the domain part
+    domain = domain?.split(",")[0];
+    if (!domain || !local) {
+      throw new Error("Invalid email");
+    }
+
+    // You can also throw an error, which will redirect the user
+    // to the error page with error=EmailSignin in the URL
+    if (identifier.split("@").length > 2) {
+      throw new Error("Only one email allowed");
+    }
+
+    return `${local}@${domain}`;
+  },
+});
+
+const credentialsProvider = CredentialsProvider({
+  // The name to display on the sign in form (e.g. "Sign in with...")
+  id: "credentials",
+  name: "Credentials",
+  // `credentials` is used to generate a form on the sign in page.
+  // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+  // e.g. domain, username, password, 2FA token, etc.
+  // You can pass any HTML attribute to the <input> tag through the object.
+  credentials: {
+    username: { label: "Username", type: "text" },
+    password: { label: "Password", type: "password" },
+  },
+  async authorize(credentials, req) {
+    // Add logic here to look up the user from the credentials supplied
+    console.log(
+      credentials,
+      process.env.ADMIN_EMAIL === credentials?.username,
+      process.env.ADMIN_PASSWORD === credentials?.password
+    );
+    if (
+      credentials?.username === process.env.ADMIN_EMAIL &&
+      credentials?.password === process.env.ADMIN_PASSWORD
+    ) {
+      console.log("JDDDDDDDIIIIDDD");
+      let user = await prisma.user.findUnique({
+        where: { email: process.env.ADMIN_EMAIL },
+      });
+
+      // Create the user if first login
+      if (!user) {
+        user = await prisma.user.create({
+          data: {
+            email: process.env.ADMIN_EMAIL,
+          },
+        });
+      }
+
+      return user;
+    } else {
+      return null;
+    }
+  },
+});
 
 export const authOptions: AuthOptions = {
   callbacks: {
@@ -57,43 +138,9 @@ export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
 
   // Configure one or more authentication providers
-  providers: [
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT
-          ? parseInt(process.env.EMAIL_SERVER_PORT)
-          : undefined,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-        secure: true,
-      },
-      from: process.env.EMAIL_FROM,
-
-      normalizeIdentifier(identifier: string): string {
-        // Get the first two elements only,
-        // separated by `@` from user input.
-        // eslint-disable-next-line prefer-const
-        let [local, domain] = identifier.toLowerCase().trim().split("@");
-        // The part before "@" can contain a ","
-        // but we remove it on the domain part
-        domain = domain?.split(",")[0];
-        if (!domain || !local) {
-          throw new Error("Invalid email");
-        }
-
-        // You can also throw an error, which will redirect the user
-        // to the error page with error=EmailSignin in the URL
-        if (identifier.split("@").length > 2) {
-          throw new Error("Only one email allowed");
-        }
-
-        return `${local}@${domain}`;
-      },
-    }),
-  ],
+  providers: process.env.NEXT_PUBLIC_MULTI_USER
+    ? [emailProvider]
+    : [credentialsProvider],
 };
 
 export const getServerAuthSession = (ctx: {
